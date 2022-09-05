26220b0e9558fd6800cd275dc794e03c578c3e8f
melatisukma1666 <melatisukma083@gmail.com>
2022-09-05 19:14:46 +0700

    Membuat UI Sign Up

--- app/src/main/java/com/example/login/SignUp.kt
+++ app/src/main/java/com/example/login/SignUp.kt
@@ -1,11 +1,20 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val btnSubmit = findViewById<Button>(R.id.btnsignup)
+
+        btnSubmit.setOnClickListener {
+            val intent = Intent(this, NextActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
