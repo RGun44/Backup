20808f31d8fd91b1dfae43075964386102fa73ef
melatisukma1666 <melatisukma083@gmail.com>
2022-09-08 15:26:59 +0700

    Ada perubahan

--- app/src/main/java/com/example/login/LoginActivity.kt
+++ app/src/main/java/com/example/login/LoginActivity.kt
@@ -4,18 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var username : TextInputLayout
+    private lateinit var password : TextInputLayout
+    lateinit var mBundle: Bundle
+
+    lateinit var vUsername: String
+    lateinit var vPassword: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        getBundle()
+        setText()
 
-        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        val btnSubmit = findViewById<Button>(R.id.btnLogin)
 
         btnSubmit.setOnClickListener{
             val intent = Intent(this, HomePenggunaActivity::class.java)
             startActivity(intent)
         }
+
+    }
+
+    fun getBundle(){
+        mBundle = intent.getBundleExtra("register")!!
+        vUsername = mBundle.getString("nama")!!
+        vPassword = mBundle.getString("nim")!!
+    }
+
+    fun setText(){
+        username = findViewById(R.id.inputLayoutUsername)
+        username.getEditText()?.setText(vUsername)
+        password = findViewById(R.id.inputLayoutPassword)
+        password.getEditText()?.setText(vPassword)
     }
 }
