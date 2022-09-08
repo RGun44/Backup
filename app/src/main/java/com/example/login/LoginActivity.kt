33160f4840427b2d287355efbe6b9b18e8daeeaa
RGun44* <ricogunawan444@gmail.com*>
2022-09-08 14:00:43 +0700

    Tambah Logo, Tulisan Logo, Vector

--- app/src/main/java/com/example/login/LoginActivity.kt
+++ app/src/main/java/com/example/login/LoginActivity.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
-class SignUp : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_log_in)
 
-        val btnSubmit = findViewById<Button>(R.id.btnSignUp)
+        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
 
-        btnSubmit.setOnClickListener {
+        btnSubmit.setOnClickListener{
             val intent = Intent(this, HomePenggunaActivity::class.java)
             startActivity(intent)
         }
